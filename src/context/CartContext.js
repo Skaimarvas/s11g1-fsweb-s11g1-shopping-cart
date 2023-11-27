933c4c94b8055161abb17d6a3182db446d290b70
skaimarvas <skaimarvas@gmail.com>
2023-11-27 16:23:53 +0300

    localStorage ürün bilgileri aktarma işlemi yapıldı.

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,18 +1,38 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContextObject = createContext();
 
-export const CartContextProvider = ({ children }) => {
+const storage = JSON.parse(localStorage.getItem("cart"));
+
+export const CartContextProvider = ({ children, local = storage }) => {
   const [cart, setCart] = useState([]);
   const addItem = (item) => {
     // verilen itemi sepete ekleyin
-    setCart((prevProd) => [...prevProd, item]);
+    setCart((prevProd) =>
+      !cart.includes(item) ? [...prevProd, item] : [...prevProd]
+    );
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  const remItem = (id) => {
+    const updatedCarts = cart.filter((prd) => id !== prd.id);
+    setCart(updatedCarts);
+    localStorage.setItem("cart", JSON.stringify(updatedCarts));
   };
   const contextCartObject = {
     cart,
     setCart,
     addItem,
+    remItem,
   };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  //   useEffect(() => {
+  //     local && setCart(local);
+  //   }, []);
 
   return (
     <CartContextObject.Provider value={contextCartObject}>
